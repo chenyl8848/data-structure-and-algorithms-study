6519a4a8e547fa49c100a21b81061ef1644dfbc4
chenyl8848 <1063415880@qq.com>
2023-10-10 09:53:56 +0800

    项目初始化

---
+++ src/main/java/com/cyl/dsa/Test.java
@@ -0,0 +1,13 @@
+package com.cyl.dsa;
+
+/**
+ * @author：Java陈序员
+ * @date：2023/10/10 9:53
+ * @description：测试类
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}

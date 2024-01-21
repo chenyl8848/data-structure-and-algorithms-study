7d5427fec3409cf71dc0b08dd117c96a0db8330d
chenyl8848 <1063415880@qq.com>
2024-01-21 22:30:23 +0800

    feat:冒泡排序

---
+++ src/main/java/com/cyl/sort/BubbleSort.java
@@ -0,0 +1,42 @@
+package com.cyl.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author：Java陈序员
+ * @date：2024/1/21 21:49
+ * @description：冒泡排序
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        // 冒泡排序时间复杂度：O(n^2)
+//        int array[] = {3, 9, -1, 10, -2};
+        int array[] = {3, 9, -1, 10, 20};
+//        int array[] = {1, 2, 3, 4, 5};
+//        int array[] = {5, 4, 3, 2, 1};
+
+        int temp = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            int count = 0;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+
+                if (array[j] > array[j + 1]) {
+                    count++;
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+
+            }
+
+            if (count == 0) {
+                break;
+            }
+
+            System.out.printf("第 %d 躺排序后的数组：%n", i + 1);
+            System.out.println(Arrays.toString(array));
+        }
+    }
+}
